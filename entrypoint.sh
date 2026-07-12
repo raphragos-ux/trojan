e4bf9c0c9a8d7a7880c2add7b274a9c2d601767f
raphragos-ux <raphragos@gmail.com>
2026-07-12 16:54:42 +0800

    Simplify entrypoint.sh by removing SSL and Nginx setup
    Removed SSL certificate generation and Nginx configuration steps from the entrypoint script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 set -e
 
-# Gumawa ng SSL cert
 openssl req -x509 -newkey rsa:4096 -keyout /app/key.key -out /app/cert.crt -days 3650 -nodes -subj "/CN=trojan-server"
 
-# Gamitin ang PORT mula sa GCP
-sed -i "s/listen 8080;/listen $PORT;/" /etc/nginx/nginx.conf
-
-# Siguraduhing may sagot sa Health Check
 echo "OK" > /usr/share/nginx/html/index.html
 
-# Simulan ang Nginx
 nginx
 
-# Simulan ang Trojan
 exec trojan -c /app/config.json
+
