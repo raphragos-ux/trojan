d49c877406bf8cd7358d45194b6c2f1caf728e9a
raphragos-ux <raphragos@gmail.com>
2026-07-12 12:36:28 +0800

    Modify entrypoint.sh for Nginx and Trojan messages
    Updated entrypoint script to include Nginx and Trojan startup messages.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -e
 
-# Gumawa ng SSL Certificate
 openssl req -x509 -newkey rsa:4096 -keyout /app/key.key -out /app/cert.crt -days 3650 -nodes -subj "/C=PH/ST=Visayas/L=Iloilo/O=FreeData/CN=virgozki.com"
 
-# Simulan ang Nginx
 nginx
 
-# Simulan ang Trojan
-echo "✅ Trojan Server nagsimula na"
+echo "✅ Nginx sa 8080 | Trojan sa 8443 | PATH: /trojan-ws"
 exec trojan -c /app/config.json
-
